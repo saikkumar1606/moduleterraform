f1dcae0c9c3f836993847791c83836d450661c3c
saikkumar1606 <ksai61587@gmail.com>
2025-07-02 15:58:30 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "ami-id" {
+  description = "passing ami-id"
+  type        = string
+}
+
+variable "key-name" {
+  description = "passing key-name"
+  type        = string
+}
+
+variable "az" {
+  description = "passing az"
+  type        = string
+}
+
+variable "ins-type" {
+  description = "passing the instype"
+  type        = string
+}
+
+variable "name" {
+  description = "passing name "
+  type        = string
+}
